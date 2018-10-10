9a96242864ab50e4e1c44a93db69cc8476580e6a
Aymeric Neveu <emploi.neveu@gmail.com>
2018-10-10 10:08:17 +0200

    exercice6P2JS

---
+++ script.js
@@ -0,0 +1,14 @@
+function rico(){
+  document.getElementById('image1').src='/home/xricoacm/Images/image1_2.jpg';
+  document.getElementById('image2').src='/home/xricoacm/Images/image2_2.jpg';
+  document.getElementById('image3').src='/home/xricoacm/Images/image3_2.jpg';
+  document.getElementById('image4').src='/home/xricoacm/Images/image4_2.jpg';
+  document.getElementById('image5').src='/home/xricoacm/Images/image5_2.jpg';
+}
+function rico1(){
+  document.getElementById("image1").src='/home/xricoacm/Images/image1.jpg';
+  document.getElementById("image2").src='/home/xricoacm/Images/image2.jpg';
+  document.getElementById("image3").src='/home/xricoacm/Images/image3.jpg';
+  document.getElementById("image4").src='/home/xricoacm/Images/image4.jpg';
+  document.getElementById("image5").src='/home/xricoacm/Images/image5.jpg';
+}
